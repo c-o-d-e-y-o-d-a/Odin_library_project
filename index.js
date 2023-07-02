258d8bb8b62e6a1c3a94e66d7b737063ef890fba
nischal gautam <nischalgautam7777@gmail.com>
2023-07-02 22:19:57 +0530

    created the basic layout for the page

---
+++ index.js
@@ -0,0 +1,35 @@
+const titleOfBook = document.getElementById('title');
+const authorName = document.getElementById('author_input');
+const read_val = document.getElementById('read_val');
+const addButton = document.getElementById('add_book');
+const addButtonForm = document.getElementById('read_val');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const books = [];
+
+function book_object(name_of_book,author,read_status){
+    this.name=name_of_book;
+    this.author=author;
+    this.read_status=read_status;
+    
+}
+
+function add_book(){
+
+
+}
