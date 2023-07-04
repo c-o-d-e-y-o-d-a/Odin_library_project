10f971c4e8c9eb9d8b8b0d166edcbc7e27bed723
nischal gautam <nischalgautam7777@gmail.com>
2023-07-04 20:52:37 +0530

    made the js part a bit better

--- index.js
+++ index.js
@@ -8,35 +8,34 @@ form.addEventListener('submit',(e)=>{
     e.preventDefault();
     const fd = new FormData(form);
 
-    const obj = Object.fromEntries(fd);
-    const newBookHtml = ` <div class="book_info">\
-    <div class="close_icon"></div>\
-    <div class="written_content">\
-        <div class="book_name">${obj.authorName}</div>\
-        <div class="author"> ${obj.titleOfBook} </div>\
-        <div class="pages_read"> ${obj.no_of_pages}</div>\
-    </div>\
-    <div class="read_unread_button"></div>\
-    </div>`;
+    obj = Object.fromEntries(fd);
+    const newBook = Object.create(book_object);
+    newBook.name=obj.authorName;
+    newBook.author=obj.titleOfBook;
+    newBook.pageCount=obj.no_of_pages;
+    bookList.push(newBook);
 
-    document.getElementById('book_storage').innerHTML += newBookHtml;
+    
 
     
 })             
 
 
+let firstBook = Object.create(book_object)
+firstBook.name='jnfjn';
+firstBook.author='donfon';
+firstBook.pageCount='sdnnfiou';
+const bookList = [firstBook,];
 
 
 
 
 
-
-const books = [];
-
-function book_object(name_of_book,author,read_status){
+function book_object(name_of_book,author,read_status,page_count){
     this.name=name_of_book;
     this.author=author;
     this.read_status=read_status;
+    this.pageCount =page_count;
     this.changeReadStatus = () =>{
         if(this.read_status==true){
             this.read_status=false;
@@ -49,9 +48,25 @@ function book_object(name_of_book,author,read_status){
 }
 
 function add_book(){
-    const newBook = Object.create(book_object);
-    newBook.name=titleOfBook.value;
-   
+    
+    
+
+
+
+}
+
+for(let i=0;i<bookList.length;i++){
+    const a = bookList[i];
+    const newBookHtml = ` <div class="book_info">\
+    <div class="close_icon"></div>\
+    <div class="written_content">\
+        <div class="book_name">${a.name}</div>\
+        <div class="author"> ${a.author} </div>\
+        <div class="pages_read"> ${a.no_of_pages}</div>\
+    </div>\
+    <div class="read_unread_button"></div>\
+    </div>`;
+    document.getElementById('book_storage').innerHTML+=newBookHtml;
 
 
 
