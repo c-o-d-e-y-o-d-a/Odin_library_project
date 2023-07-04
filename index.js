8bd931a483a94fe04650d736ee98133c825985c9
nischal gautam <nischalgautam7777@gmail.com>
2023-07-04 23:22:33 +0530

    did some css work

--- index.js
+++ index.js
@@ -3,36 +3,8 @@ const addButton = document.getElementById('add_book');
 const addButtonForm = document.getElementById('read_val');
 
 
-
-form.addEventListener('submit',(e)=>{
-    e.preventDefault();
-    const fd = new FormData(form);
-
-    obj = Object.fromEntries(fd);
-    const newBook = Object.create(book_object);
-    newBook.name=obj.authorName;
-    newBook.author=obj.titleOfBook;
-    newBook.pageCount=obj.no_of_pages;
-    bookList.push(newBook);
-
-    
-
-    
-})             
-
-
-let firstBook = Object.create(book_object)
-firstBook.name='jnfjn';
-firstBook.author='donfon';
-firstBook.pageCount='sdnnfiou';
-const bookList = [firstBook,];
-
-
-
-
-
 function book_object(name_of_book,author,read_status,page_count){
-    this.name=name_of_book;
+    this.name1=name_of_book;
     this.author=author;
     this.read_status=read_status;
     this.pageCount =page_count;
@@ -46,6 +18,51 @@ function book_object(name_of_book,author,read_status,page_count){
     }
     
 }
+let firstBook = Object.create(book_object)
+firstBook.name1='jnfjn';
+firstBook.author='donfon';
+firstBook.pageCount='sdnnfiou';
+const bookList = [firstBook,];
+
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const fd = new FormData(form);
+
+    obj = Object.fromEntries(fd);
+    
+    
+    bookList.push(new book_object(obj.titleOfBook,obj.authorName,obj.c));
+
+    for(let i=0;i<bookList.length;i++){
+        const a = bookList[i];
+        const newBookHtml = ` <div class="book_info">\
+        <div class="close_icon"></div>\
+        <div class="written_content">\
+            <div class="book_name">${a.name1}</div>\
+            <div class="author"> ${a.author} </div>\
+            <div class="pages_read"> ${a.pageCount}</div>\
+        </div>\
+        <div class="read_unread_button"></div>\
+        </div>`;
+        document.getElementById('book_storage').innerHTML+=newBookHtml;
+    
+    
+    
+    }
+
+    
+
+    
+})             
+
+
+
+
+
+
+
+
 
 function add_book(){
     
@@ -55,20 +72,5 @@ function add_book(){
 
 }
 
-for(let i=0;i<bookList.length;i++){
-    const a = bookList[i];
-    const newBookHtml = ` <div class="book_info">\
-    <div class="close_icon"></div>\
-    <div class="written_content">\
-        <div class="book_name">${a.name}</div>\
-        <div class="author"> ${a.author} </div>\
-        <div class="pages_read"> ${a.no_of_pages}</div>\
-    </div>\
-    <div class="read_unread_button"></div>\
-    </div>`;
-    document.getElementById('book_storage').innerHTML+=newBookHtml;
 
-
-
-}
 
